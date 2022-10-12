e68a73093e97b0087e7d49d9c63e920316b4f192
SAISRAVAN <oletisravan@gmail.com>
2022-10-12 13:42:53 +0530

    Sravan : Implement Hide the dice image

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+//All elements 
+const score0 = document.querySelector("#score--0");
+const score1 = document.getElementById("score--1");
+const diceEl = document.querySelector('.dice');
+
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add('hidden');
+
+
+
